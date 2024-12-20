970ee000c41caed14ffa6a192467df70a3f9fc33
Mustapha <mustapha.hammouny@gmail.com>
2024-12-20 12:02:36 +0100

    feat: add to cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,21 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const plant = state.items.find(
+                (item) => item.name === action.payload.name
+            );
 
-    
+            if (plant) {
+                plant.quantity++;
+            } else {
+                state.items.push({ ...plant, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {},
+        updateQuantity: (state, action) => {},
     },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
